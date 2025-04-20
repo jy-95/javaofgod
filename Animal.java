3d283b6c8282830f20ca4340cea40c890cc34095
jy-95 <jiiyon.lee@gmail.com>
2025-04-20 13:44:19 +0900

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,19 @@
+package c.inheritance;
+
+public class Animal {
+	String name;
+	String hairColor;
+	int legCount;
+	int iq;
+	boolean hasWing;
+	
+	
+	public void move() {
+		System.out.println("터벅터벅");
+	}
+	
+	public void eatFood() {
+		System.out.println("념념");
+	}
+
+}
