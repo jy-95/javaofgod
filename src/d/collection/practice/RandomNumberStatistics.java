ace9482ee9270fb85fc8366d1e7b01f062b311a1
jy-95 <jiiyon.lee@gmail.com>
2025-05-14 17:50:10 +0900

    thread

--- src/d/collection/practice/RandomNumberStatistics.java
+++ src/d/collection/practice/RandomNumberStatistics.java
@@ -9,19 +9,34 @@ public class RandomNumberStatistics {
 	Hashtable<Integer, Integer> hashtable = new Hashtable<Integer, Integer>();
 	
 	public static void main(String[] args) {
-
+		RandomNumberStatistics sample = new RandomNumberStatistics();
+		sample.getRandomNumberStatistics();
 	}
 	
 	public void getRandomNumberStatistics() {
-		for (int i = 0; i < 5000; i++) {
-			Random random = new Random();
-			int tempNumber = random.nextInt();
+		Random random = new Random();
+		int runcount = DATA_BOUNDARY * 100;
+		for (int i = 0; i < runcount; i++) {
+			int tempNumber = random.nextInt(DATA_BOUNDARY)+1;
 			putCurrentNumber(tempNumber);
-			
 		}
+		printStatistics();
 	}
 	public void putCurrentNumber(int tempNumber) {
-		
+		if(hashtable.containsKey(tempNumber)) {
+			hashtable.put(tempNumber,hashtable.get(tempNumber)+1);
+		}else{
+			hashtable.put(tempNumber, 1);
+		}
+	}
+	public void printStatistics() {
+		Set<Integer> keySet = hashtable.keySet();
+		for (Integer key : keySet) {
+			int count = hashtable.get(key);
+			System.out.print(key + "=" + count + "\t");
+			if(key%10-1==0)
+				System.out.println();
+		}
 	}
 
 }
